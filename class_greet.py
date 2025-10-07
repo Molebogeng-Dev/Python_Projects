18b1898fb8301a3b2582e3b44a61006cf063811d
Molebogeng <business.molebogeng@gmail.com>
2025-10-07 14:15:06 +0200

    Update class_greet.py

--- class_greet.py
+++ class_greet.py
@@ -1,6 +1,8 @@
 class Dumela:
-  def __init__(self,name=''):
-    self.name=name
+  def __init__(self):
+    self.name=input('Enter name: ')
 
   def __str__(self):
     return f'Dumela {self.name}.'
+
+print(Dumela())
