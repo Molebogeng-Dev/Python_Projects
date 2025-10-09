ab17d02c34576c210eb821ddce0c1e135d1cc3cc
Molebogeng <business.molebogeng@gmail.com>
2025-10-09 17:10:05 +0200

    Done  discount.py

--- discount.py
+++ discount.py
@@ -3,6 +3,5 @@ def calculate_discount(num1,num2):
         return float(num1 - (num1 * 90/100))
     elif num1 > 0 and num2 > 0:
         return float(num1 - (num1 * num2/100))
-    else:
-        raise(ValueError)
+    raise(ValueError)
 
