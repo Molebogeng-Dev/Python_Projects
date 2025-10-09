e79796d1e3dc41181d347786e80d016d7bb56ef7
Molebogeng <business.molebogeng@gmail.com>
2025-10-09 17:08:14 +0200

    basic discount code

---
+++ discount.py
@@ -0,0 +1,8 @@
+def calculate_discount(num1,num2):
+    if num1 > 0 and num2 > 90:
+        return float(num1 - (num1 * 90/100))
+    elif num1 > 0 and num2 > 0:
+        return float(num1 - (num1 * num2/100))
+    else:
+        raise(ValueError)
+
