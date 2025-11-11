4c5fd6f14900bae6f01da3f5a5550cd3182ab8fc
Molebogeng <business.molebogeng@gmail.com>
2025-11-11 20:09:08 +0000

    not done

--- _.py
+++ _.py
@@ -1,17 +1,1 @@
-import sys
-
-class Rover:
-    
-    def __init__(self, path):
-        try:
-            with open(path) as file:
-                self.file= file.readlines()
-        except FileNotFound:
-            sys.exit('Cannot open file')
-
-
-def main():
-    rover= Rover(sys.agrv[1])
-
-if __name__ == "__main__":
-    main()
+print(0+(-10))
--- _/_.py
+++ _/_.py
@@ -17,19 +17,21 @@ class Rover:
                 }
         self.x_y=[0,0]
         
-        print("I'm at (0, 0) facing 0 degrees")
+        print("I'm at (0.00, 0.00) facing 0.00 degrees")
 
 
   
-    def calculations(self,campus,meters):
-        if self.degrees < 90:
-            self.x_y=[round(campus[0]*meters)+self.x_y[0],round(campus[1]*meters)+self.x_y[1]]
-        elif self.degrees < 180:
-            self.x_y=[round(campus[0]*meters)+self.x_y[0],round(campus[1]*meters)-self.x_y[1]]
-        elif self.degrees < 270:
-            self.x_y=[round(campus[0]*meters)-self.x_y[0],round(campus[1]*meters)-self.x_y[1]]
+    def calculations(self,campus,meters,direction):
+        if self.degrees <= 90:
+            # if direction == 'backwards':
+            #     self.x_y[1]=int(str(f'-{self.x_y[1]}'))
+            self.x_y=[round(campus[0]*meters,2)+self.x_y[0],round(campus[1]*meters,2)+self.x_y[1]]
+        elif self.degrees <= 180:
+            self.x_y=[round(campus[0]*meters,2)+self.x_y[0],round(campus[1]*meters,2)-self.x_y[1]]
+        elif self.degrees <= 270:
+            self.x_y=[round(campus[0]*meters,2)-self.x_y[0],round(campus[1]*meters,2)-self.x_y[1]]
         else:
-            self.x_y=[round(campus[0]*meters)-self.x_y[0],round(campus[1]*meters)+self.x_y[1]]
+            self.x_y=[round(campus[0]*meters,2)-self.x_y[0],round(campus[1]*meters,2)+self.x_y[1]]
        
 
 
@@ -40,10 +42,10 @@ class Rover:
                 for point in self.cat_p:
 
                     if point <= self.degrees:
-                        self.calculations(self.cat_p[point],int(ls_ins[1]))
+                        self.calculations(self.cat_p[point],int(ls_ins[1]),ls_ins[3])
                         
-                        print(f"Moving {ls_ins[1]} meters forward (instruction {i+1})")
-                        print(f"I'm at ({self.x_y[0]}, {self.x_y[1]}) facing {self.degrees} degrees")
+                        print(f"Moving {float(ls_ins[1])} meters {ls_ins[3]} (instruction {i+1}.00)")
+                        print(f"I'm at ({self.x_y[0]}, {self.x_y[1]}) facing {float(self.degrees)} degrees")
                         break 
 
             elif 'degrees' in ls_ins and len(ls_ins) == 4:
@@ -56,8 +58,8 @@ class Rover:
                     while self.degrees < 0:
                         self.degrees+=360
 
-                print(f"Turning {ls_ins[1]} degrees {ls_ins[3]} (instruction {i+1})")
-                print(f"I'm at ({self.x_y[0]}, {self.x_y[1]}) facing {self.degrees} degrees")
+                print(f"Turning {float(ls_ins[1])} degrees {ls_ins[3]} (instruction {i+1}.00)")
+                print(f"I'm at ({self.x_y[0]}, {self.x_y[1]}) facing {float(self.degrees)} degrees")
 
             else:
                 print(f"I've encountered an instruction I don't understand, aborting (instruction {i+1})")        
