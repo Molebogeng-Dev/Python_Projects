e9469a3c31f93feb42e319dff8fd8c91aa230972
Molebogeng <business.molebogeng@gmail.com>
2025-11-21 12:53:03 +0000

    not done with _

--- _/_.py
+++ _/_.py
@@ -1,17 +1,79 @@
 import sys
 
-# class Cross_hatch:
-#     #find a cell that a number can fill one block
-#     @staticmethod
-#     def hidden_single(numbers):
-#         for num in numbers:
+class Techniques:
+    def __init__(self,sudoku_ls):
+        self.sudoku_numbers=[9,8,7,6,5,4,3,2,1]
+        self.sudoku_ls= sudoku_ls
 
-        
-#     def naked_single(self):
-#         ...
+    #finding a row that needs candidate/s
+    def possible_nums(self,r_c,cell):
+        #row
+        current_row= self.sudoku_ls['rows'][r_c]
+        for i in range(len(current_row)):
+            try:
+                #row
+                if current_row[i] == 0:
+                    current_row[i] =[]
+                        
+                    for num in self.sudoku_numbers:
+                        try:
+                            if num not in current_row and num not in cell:
+                                current_row[i].append(num)
+                        except TypeError:
+                            pass
+                
+                    #column
+                    current_column= self.sudoku_ls['cols'][i]
+                    for i in range(len(current_column)):
+                        try:
+                            if current_column[i] == 0:
+                                current_column[i]= []
+                                
+                                for num in self.sudoku_numbers:
+                                    try:
+                                        if num not in current_column and num not in cell or num in current_row[i]:
+                                            current_column[i].append(num)
+                                    except TypeError:
+                                        pass
+                                if len(current_column[i]) == 1:
+                                    current_column[i]= current_column[i][0]
+                                current_row[i] = current_column[i]
+                                return current_column[i]
+                        except TypeError:
+                            pass
+            except TypeError:
+                pass
+
+    #finding a cell that needs candidate/s
+    def cell(self):
+        #row and column changer
+        row_counter=-1
+        for cell in self.sudoku_ls['cells']:
+            for index, num in enumerate(cell):
+                if index % 3 == 0:
+                    row_counter+=1
+                    if row_counter == len(self.sudoku_ls['rows']):
+                        row_counter=0
+                if num == 0:
+                    cell[index] = self.possible_nums(row_counter,cell)
+        print('CELLS:\n',self.sudoku_ls['cells'],'\n==================\nROWS:\n',self.sudoku_ls['rows'],'\n==================\nCOLS:\n',self.sudoku_ls['cols'])
+
+                    # self.col(row_i[1])
+                
+
+
+
 
 def numbers(puzzle):
-    return [i.split() for i in puzzle]
+    numbers_ls,ls=[],[]
+    for nums in puzzle:
+        for num in nums:
+            if num.isdigit():
+                ls.append(int(num))
+            if len(ls) == 9:
+                numbers_ls.append(ls)
+                ls=[]
+    return numbers_ls
 
 def organized(numbers):
     sudoku= {'rows': [],'cols': [],'cells': []}
@@ -19,7 +81,7 @@ def organized(numbers):
     sudoku['rows']= numbers
     #columns
     num= 0
-    while num != len(numbers) - 1:
+    while num != len(numbers):
         ls=[]
         for i in range(len(numbers)):
             ls.append(numbers[i][num])
@@ -38,24 +100,25 @@ def organized(numbers):
         r1+=3
         r2+=3
 
+    # print('CELLS:\n',sudoku['cells'],'\n==================\nROWS:\n',sudoku['rows'],'\n==================\nCOLS:\n',sudoku['cols'])
     return sudoku
 
-
-
 def main():
-    sudoku_numbers=''
+    sudoku_crc=''
     # if len(sys.argv) != 4:
     #     sys.exit("Incorrect arguments")
         
     with open('puzzle.txt') as puzzle:#sys.argv[1]
-        sudoku_numbers= organized(numbers(puzzle))
-
+        sudoku_crc= Techniques(organized(numbers(puzzle)))
     
+    sudoku_crc.cell()
+
+
     # if sys.argv[2] == '-o':
     #     #printing table with numbers (remove later).
     #     with open(sys.argv[3], 'w') as sovled_puzzle:
-    #         for num in range(len(sudoku_numbers)):
-    #             for d,i in enumerate(sudoku_numbers[num]):
+    #         for num in range(len(sudoku_crc)):
+    #             for d,i in enumerate(sudoku_crc[num]):
     #                 if (d+1)%3 == 0 and d != 0:
     #                     sovled_puzzle.write(f'{i}|')
     #                 else:
