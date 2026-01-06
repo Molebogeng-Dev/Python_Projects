5ec4b6a6feb5c7f89b35ef4faaeb1aee725a3999
Molebogeng <business.molebogeng@gmail.com>
2026-01-06 16:43:38 +0000

    pc_update

--- datascience/podcast_templet.py
+++ datascience/podcast_templet.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
+class 
+'''ToDo: finish this function that will update everything in the df '''
+def update_all(df):
+    titles= df.columns
+    for row in range(len(df)):
+        for col in range(len(df.columns)):
+            df.iloc[row][col]= input(f"Enter {titles[col]}: ")
+        print(f"{df.iloc[row][1]}'s info updated")
+    print("Data updated")
+
+             
 episodes=[{"Title": ":______", "Guest": ":___", "Main_topic": ":__________"},
           {"Title": ":______", "Guest": ":___", "Main_topic": ":__________"},
           {"Title": ":______", "Guest": ":___", "Main_topic": ":__________"},
@@ -9,3 +20,4 @@ index_name=[f"Number{i+1}" for i in range(len(episodes)) ]
 
 df = pd.DataFrame(episodes, index= index_name)
 
+print(df)
